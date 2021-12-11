75801bab47afdca2eb19421fac9ab30b227e0251
tails618 <asher.grossman@gmail.com>
2021-12-11 16:03:11 -0600

    lmao we didn't do much

--- Cache-Money-Code-Training/src/main/java/frc/robot/SubGroups/Arm.java
+++ Cache-Money-Code-Training/src/main/java/frc/robot/SubGroups/Arm.java
@@ -1,5 +1,16 @@
 package frc.robot.SubGroups;
 
-public class Arm {
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Arm extends Subsystem{
+    public static Servo armMotor = new Servo(0);
+
+    @Override
+    protected void initDefaultCommand() {
+        // TODO Auto-generated method stub
+        
+    }
+
     
 }
---
+++ Cache-Money-Code-Training/src/main/java/frc/robot/SubGroups/ArmCommand.java
@@ -0,0 +1,19 @@
+package frc.robot.SubGroups;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class ArmCommand extends Command{
+
+    @Override
+    protected void execute(){
+        super.execute();
+    }
+    @Override
+    protected boolean isFinished() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
