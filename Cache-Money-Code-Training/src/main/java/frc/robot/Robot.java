5feb3e656adb5ed0bcfd868a07a029cb47bffa1a
ademirtas04 <59454552+ademirtas04@users.noreply.github.com>
2021-12-11 16:03:20 -0600

    Updates after Training

--- Cache-Money-Code-Training/src/main/java/frc/robot/Robot.java
+++ Cache-Money-Code-Training/src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
--- Cache-Money-Code-Training/src/main/java/frc/robot/SubGroups/Lift.java
+++ Cache-Money-Code-Training/src/main/java/frc/robot/SubGroups/Lift.java
@@ -1,5 +1,45 @@
 package frc.robot.SubGroups;
 
-public class Lift {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Lift extends Subsystem{
+    public static VictorSPX climbMotor = new VictorSPX(0);
+    //???
+    public static VictorSPX climbMotorRetract = new VictorSPX(0);
+    @Override
+    protected void initDefaultCommand() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public static void extendClimbOverDeltaT(double deltaT){
+        climbMotor.set(ControlMode.PercentOutput, demand);
+        
+        boolean buttonActivated = true;
+        //if ( button activated ) 
+        while (buttonActivated){
+            // int armDistance = 50;
+            // double speed = armDistance / deltaT
+        }
+    }
+
+    public static void retractClimbOverDeltaT(double deltaT){
+        /*
+        while(something.getButtonDown?(something)==true){
+            climbMotorRetract.somethingLikeSetAngle();
+            
+        }
+        
+        */
+
+    }
+
+
+
+    
     
 }
