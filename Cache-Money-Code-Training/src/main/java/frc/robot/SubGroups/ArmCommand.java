e106295560d393a9264a23ec6f7f1d3e8b03e5ae
ademirtas04 <59454552+ademirtas04@users.noreply.github.com>
2021-12-18 14:32:50 -0600

    quick commits

--- Cache-Money-Code-Training/src/main/java/frc/robot/SubGroups/ArmCommand.java
+++ Cache-Money-Code-Training/src/main/java/frc/robot/SubGroups/ArmCommand.java
@@ -1,19 +1,21 @@
 package frc.robot.SubGroups;
 
-import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class ArmCommand extends Command{
+public class ArmCommand extends Command {
 
     @Override
-    protected void execute(){
+    protected void execute() {
+        // TODO Auto-generated method stub
         super.execute();
     }
+
     @Override
     protected boolean isFinished() {
         // TODO Auto-generated method stub
         return false;
     }
+
+
     
 }
--- Cache-Money-Code-Training/src/main/java/frc/robot/SubGroups/Lift.java
+++ Cache-Money-Code-Training/src/main/java/frc/robot/SubGroups/Lift.java
@@ -10,6 +10,7 @@ public class Lift extends Subsystem{
     public static VictorSPX climbMotor = new VictorSPX(0);
     //???
     public static VictorSPX climbMotorRetract = new VictorSPX(0);
+
     @Override
     protected void initDefaultCommand() {
         // TODO Auto-generated method stub
@@ -20,15 +21,16 @@ public class Lift extends Subsystem{
         climbMotor.set(ControlMode.PercentOutput, demand);
         
         boolean buttonActivated = true;
+        // int armDistance = 50;
+        // double speed = armDistance / deltaT;
         //if ( button activated ) 
         while (buttonActivated){
-            // int armDistance = 50;
-            // double speed = armDistance / deltaT
+            //extendArm(speed);
         }
     }
 
     public static void retractClimbOverDeltaT(double deltaT){
-        /*
+        /*what
         while(something.getButtonDown?(something)==true){
             climbMotorRetract.somethingLikeSetAngle();
             
